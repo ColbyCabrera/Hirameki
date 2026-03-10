305fa107f840973682bc77620335c0fb34791303
ColbyCabrera <gdthyispro@gmail.com>
2026-03-10 13:04:48 -0400

    Clean up `AnalyticsTest.kt` syntax
    - Removed unnecessary `?` from `whenever(analytics)` call
    - Used an underscore for the unused exception variable in the `catch` block

--- AnkiDroid/src/test/java/com/ichi2/anki/AnalyticsTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/AnalyticsTest.kt
@@ -117,10 +117,10 @@ class AnalyticsTest {
 
                 // prepare analytics so we can inspect what happens
                 val spyHit = spy(ExceptionHit())
-                doReturn(spyHit).whenever(analytics)?.exception()
+                doReturn(spyHit).whenever(analytics).exception()
                 try {
                     UsageAnalytics.sendAnalyticsException(grandparentException, false)
-                } catch (e: Exception) {
+                } catch (_: Exception) {
                     // do nothing - this is expected because UsageAnalytics isn't fully initialized
                 }
                 verify(grandparentException).cause
