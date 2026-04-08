f58f35b2ac0f68838850bd9dd39856eb60c6d402
ColbyCabrera <gdthyispro@gmail.com>
2026-04-08 12:27:09 -0400

    Remove `JvmTest` inheritance from `LanguageUtilsTest`
    - Removed the unused `JvmTest` import and base class inheritance from `LanguageUtilsTest`.

--- AnkiDroid/src/test/java/com/ichi2/utils/LanguageUtilsTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/utils/LanguageUtilsTest.kt
@@ -19,7 +19,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.collect.Sets
 import com.ichi2.utils.LanguageUtilsTest.Companion.CURRENT_LANGUAGES
 import com.ichi2.utils.LanguageUtilsTest.Companion.PREVIOUS_LANGUAGES
-import com.ichi2.testutils.JvmTest
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers
 import org.junit.Ignore
@@ -27,7 +26,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class LanguageUtilsTest : JvmTest() {
+class LanguageUtilsTest {
     @Test
     @Ignore("temp ignore - languages were removed for 18980")
     fun testNoLanguageIsRemoved() {
