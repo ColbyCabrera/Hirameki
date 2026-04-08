2b3c719cc1cf9022ac4872263ad9df42ae1d2132
ColbyCabrera <gdthyispro@gmail.com>
2026-04-07 23:13:19 -0400

    Remove redundant `Editable.textAsIntOrNull` extension in `EditTextUtils.kt`
    - Removed the `Editable.textAsIntOrNull` extension function as it was redundant with the existing `CharSequence.textAsIntOrNull` implementation.
    - Removed the unused `android.text.Editable` import.

--- AnkiDroid/src/main/java/com/ichi2/utils/EditTextUtils.kt
+++ AnkiDroid/src/main/java/com/ichi2/utils/EditTextUtils.kt
@@ -16,7 +16,6 @@
 
 package com.ichi2.utils
 
-import android.text.Editable
 import android.widget.EditText
 
 /** Moves the cursor to the end of the [EditText] */
@@ -29,7 +28,5 @@ fun EditText.moveCursorToEnd() = setSelection(text?.length ?: 0)
  * note: "1.0" returns `null`
  */
 fun EditText.textAsIntOrNull() = this.text?.textAsIntOrNull()
-
-fun Editable.textAsIntOrNull() = this.toString().toIntOrNull()
 
 fun CharSequence.textAsIntOrNull() = this.toString().toIntOrNull()
