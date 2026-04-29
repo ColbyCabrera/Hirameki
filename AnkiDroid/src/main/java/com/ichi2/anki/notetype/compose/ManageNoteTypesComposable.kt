8079df4970b658b0edadef57ec6946c9aafa9c3f
ColbyCabrera <gdthyispro@gmail.com>
2026-04-29 19:15:14 -0400

    Remove unused import in `ManageNoteTypesComposable.kt`
    - Removed the unused `WindowInsets` import.

--- AnkiDroid/src/main/java/com/ichi2/anki/notetype/compose/ManageNoteTypesComposable.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/notetype/compose/ManageNoteTypesComposable.kt
@@ -25,7 +25,6 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
