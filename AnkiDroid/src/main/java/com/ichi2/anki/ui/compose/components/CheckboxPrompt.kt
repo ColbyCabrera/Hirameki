8e934c344b7c1740292bcd6752a1233f136ac466
ColbyCabrera <gdthyispro@gmail.com>
2026-05-14 00:08:10 -0400

    Improve haptic feedback in `CheckboxPrompt`
    - Updated the `toggleable` modifier in `CheckboxPrompt` to use `HapticFeedbackType.ToggleOn` and `HapticFeedbackType.ToggleOff` based on the component's state, replacing the previous generic `TextHandleMove` feedback.
    - Added the standard project license header to `CheckboxPrompt.kt`.

--- AnkiDroid/src/main/java/com/ichi2/anki/ui/compose/components/CheckboxPrompt.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/ui/compose/components/CheckboxPrompt.kt
@@ -1,3 +1,18 @@
+/* **************************************************************************************
+ * Copyright (c) 2026 Colby Cabrera <colbycabrera@gmail.com>                            *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 3 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
 package com.ichi2.anki.ui.compose.components
 
 import androidx.compose.foundation.layout.Row
@@ -31,7 +46,9 @@ fun CheckboxPrompt(
             .fillMaxWidth()
             .toggleable(value = isChecked, role = Role.Checkbox, onValueChange = {
                 onCheckedChange(it)
-                haptic.performHapticFeedback(HapticFeedbackType.TextHandleMove)
+                haptic.performHapticFeedback(
+                    if (it) HapticFeedbackType.ToggleOn else HapticFeedbackType.ToggleOff
+                )
             })
             .padding(top = 8.dp, start = horizontalPadding, end = horizontalPadding),
         verticalAlignment = Alignment.CenterVertically
